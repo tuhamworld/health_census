923937d1f12431947c13672b2e9122b3bfc8e978
Tunde Abdulhamid Sanusi <52820977+tuhamworld@users.noreply.github.com>
2026-02-04 12:37:17 +0100

    chore: minor

--- health_analysis.js
+++ health_analysis.js
@@ -24,37 +24,37 @@ function resetForm() {
 }
 
 function searchCondition() {
-    const input = document.getElementById('conditionInput').value.toLowerCase();
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
+  const input = document.getElementById("conditionInput").value.toLowerCase();
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = "";
 
-    fetch('health_analysis.json')
-        .then(response => response.json())
-        .then(data => {
-            const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+  fetch("health_analysis.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const condition = data.conditions.find(
+        (item) => item.name.toLowerCase() === input,
+      );
 
-            if (condition) {
-                const symptoms = condition.symptoms.join(',');
+      if (condition) {
+        const symptoms = condition.symptoms.join(",");
 
-                const prevention = condition.prevention.join(',');
-                const treatment = condition.treatment;
-                resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-                resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
-                resultDiv.innerHTML += `<p><strong>Symptoms: </strong> ${symptoms}</p>`;
-                resultDiv.innerHTML += `<p><strong>Prevention: </strong> ${prevention}</p>`;
-                resultDiv.innerHTML += `<p><strong>Treatment: <strong> ${treatment}</p>`;
-            } else {
-                resultDiv.innerHTML = 'Condition not found.';
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            resultDiv.innerHTML = 'An error occured while fetching data.';
-        });
+        const prevention = condition.prevention.join(",");
+        const treatment = condition.treatment;
+        resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+        resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+        resultDiv.innerHTML += `<p><strong>Symptoms: </strong> ${symptoms}</p>`;
+        resultDiv.innerHTML += `<p><strong>Prevention: </strong> ${prevention}</p>`;
+        resultDiv.innerHTML += `<p><strong>Treatment: <strong> ${treatment}</p>`;
+      } else {
+        resultDiv.innerHTML = "Condition not found.";
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      resultDiv.innerHTML = "An error occured while fetching data.";
+    });
 }
-btnSearch.addEventListener('click', searchCondition)
-
-
+btnSearch.addEventListener("click", searchCondition);
 
 function generateReport() {
   const numPatients = patients.length;
